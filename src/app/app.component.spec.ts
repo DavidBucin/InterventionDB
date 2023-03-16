cf06176a94cccfb4668c40569078a950ae7fc9bb
David Bucin <2132186@cmontmorency.qc.ca>
2023-03-16 14:52:44 -0400

    lol

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Intervention'`, () => {
+  it(`should have as title 'InterventionDB'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Intervention');
+    expect(app.title).toEqual('InterventionDB');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Intervention app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('InterventionDB app is running!');
   });
 });
