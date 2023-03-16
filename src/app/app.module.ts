2c59e3941770d6bd79195e0651cad17ec71ed1ea
David Bucin <2132186@cmontmorency.qc.ca>
2023-03-16 15:48:58 -0400

    TP6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BienvenueComponent } from './bienvenue/bienvenue.component';
+import { AccueilComponent } from './accueil/accueil.component';
+import { ProblemeComponent } from './probleme/probleme.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BienvenueComponent,
+    AccueilComponent,
+    ProblemeComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path: 'accueil', component: AccueilComponent },
+      { path: 'probleme', component: ProblemeComponent },
+      { path: '', redirectTo: 'accueil', pathMatch: 'full' },
+      { path: '**', redirectTo: 'accueil', pathMatch: 'full' },
+    ]),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
