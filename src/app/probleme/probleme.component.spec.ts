e14cdfcaf519fd3a14f1a689009b84fd0d14114e
David Bucin <2132186@cmontmorency.qc.ca>
2023-05-04 13:46:25 -0400

    TP12

--- src/app/probleme/probleme.component.spec.ts
+++ src/app/probleme/probleme.component.spec.ts
@@ -2,6 +2,8 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { ProblemeComponent } from './probleme.component';
+import { ProblemeService } from './probleme.service';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('ProblemeComponent', () => {
   let component: ProblemeComponent;
@@ -9,10 +11,10 @@ describe('ProblemeComponent', () => {
 
   beforeEach(async () => {
     TestBed.configureTestingModule({
-      imports: [ReactiveFormsModule],
-      declarations: [ ProblemeComponent ]
-    })
-    .compileComponents();
+      imports: [ReactiveFormsModule, HttpClientModule],
+      declarations: [ProblemeComponent],
+      providers: [ProblemeService],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(ProblemeComponent);
     component = fixture.componentInstance;
@@ -23,46 +25,145 @@ describe('ProblemeComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it("#1 | Zone PRÉNOM invalide avec 2 caractères", () => {
-    let zone = component.problemeForm.controls["prenom"];
-    zone.setValue("a".repeat(2));
-    expect(zone.invalid).toBeTruthy();
-  })
-
-
-  it("#2 | Zone PRÉNOM valide avec 3 caractères", () => {
-    let zone = component.problemeForm.controls["prenom"];
-    zone.setValue("a".repeat(3));
-    expect(zone.valid).toBeTruthy();
-  })
-
-
-  it("#3 | Zone PRÉNOM valide avec 200 caractères", () => {
-    let zone = component.problemeForm.controls["prenom"];
-    zone.setValue("a".repeat(200));
-    expect(zone.valid).toBeTruthy();
-  }); 
-
-  it("#4 | Zone PRÉNOM invalide avec aucune valeur", () => {
-    let zone = component.problemeForm.controls["prenom"];
-    zone.setValue("");
+  it('#1 | Zone PRÉNOM invalide avec 2 caractères', () => {
+    let zone = component.problemeForm.controls['prenom'];
+    zone.setValue('a'.repeat(2));
     expect(zone.invalid).toBeTruthy();
   });
-  
-  it("#5 | Zone PRÉNOM valide avec 10 espaces", () => {
-    let zone = component.problemeForm.controls["prenom"];
-    zone.setValue(" ".repeat(10));
+
+  it('#2 | Zone PRÉNOM valide avec 3 caractères', () => {
+    let zone = component.problemeForm.controls['prenom'];
+    zone.setValue('a'.repeat(3));
+    expect(zone.valid).toBeTruthy();
+  });
+
+  it('#3 | Zone PRÉNOM valide avec 200 caractères', () => {
+    let zone = component.problemeForm.controls['prenom'];
+    zone.setValue('a'.repeat(200));
+    expect(zone.valid).toBeTruthy();
+  });
+
+  it('#4 | Zone PRÉNOM invalide avec aucune valeur', () => {
+    let zone = component.problemeForm.controls['prenom'];
+    zone.setValue('');
     expect(zone.invalid).toBeTruthy();
-  })
-  
-  it("#6 | Zone PRÉNOM valide avec 2 espaces et 1 caractère", () => {
-    let zone = component.problemeForm.controls["prenom"];
-    zone.setValue("a ");
+  });
+
+  it('#5 | Zone PRÉNOM valide avec 10 espaces', () => {
+    let zone = component.problemeForm.controls['prenom'];
+    zone.setValue(' '.repeat(10));
     expect(zone.invalid).toBeTruthy();
-  }) 
-    
+  });
+
+  it('#6 | Zone PRÉNOM valide avec 2 espaces et 1 caractère', () => {
+    let zone = component.problemeForm.controls['prenom'];
+    zone.setValue('a ');
+    expect(zone.invalid).toBeTruthy();
+  });
+
+  it('#15 | Zone TELEPHONE est désactivée quand ne pas me notifier', () => {
+    component.gestionNotification('');
+
+    let zone = component.problemeForm.get('telephone');
+    expect(zone.disabled).toBeTrue();
+  });
+
+  it('#16 | Zone TELEPHONE est vide quand ne pas me notifier', () => {
+    component.gestionNotification("");
+
+    let zone = component.problemeForm.get('telephone');
+
+    zone.setValue('');
+
+    expect(zone.value).toBe('');
+  });
+
+  it('#17 | Zone ADRESSE COURRIEL est désactivée quand ne pas me notifier', () => {
+    component.gestionNotification("");
+
+    let zone = component.problemeForm.get('courrielGroup.courriel');
+
+    expect(zone.disabled).toBeTrue();
+  });
+
+  it('#18 | Zone CONFIRMER COURRIEL est désactivée quand ne pas me notifier', () => {
+    component.gestionNotification("");
+
+    let zone = component.problemeForm.get('courrielGroup.courrielConfirmation');
+
+    expect(zone.disabled).toBeTrue();
+  });
+  it('#19 | Zone TELEPHONE est désactivée quand ne pas me notifier', () => {
+    component.gestionNotification('pasnotification');
+    let zone = component.problemeForm.get('telephone');
+    expect(zone.status).toEqual('DISABLED'); 
+  });
+
+  it('#20 | Zone ADRESSE COURRIEL est activée quand notifier par courriel', () =>{
+    component.gestionNotification('courriel');
+
+    let zone = component.problemeForm.get('courrielGroup.courriel')
+    expect(zone.enabled).toBeFalse();
+  });
+  it('#21 | Zone CONFIRMER COURRIEL est activée quand notifier par courriel', () =>{
+    component.gestionNotification('courriel');
+
+    let zone = component.problemeForm.get('courrielGroup.courrielConfirmation')
+    expect(zone.disabled).toBeTrue();
+  });
+  it('#22 | Zone ADRESSE COURRIEL est invalide sans valeur quand notifier par courriel', () =>{
+    component.gestionNotification('courriel');
+
+    let zone = component.problemeForm.get('courrielGroup.courriel')
+    expect(zone.value).toBeNull();
+  });
+  it('#23 | Zone CONFIRMER COURRIEL est invalide sans valeur quand notifier par courriel', () =>{
+    component.gestionNotification('courriel');
+
+    let zone = component.problemeForm.get('courrielGroup.courrielConfirmation')
+    expect(zone.value).toBeNull();
+  });
+  it('#24 | Zone ADRESSE COURRIEL est invalide avec un format non conforme', () =>{
+    component.gestionNotification('courriel');
+    let zoneCourriel = component.problemeForm.get('courrielGroup.courriel')
+    zoneCourriel.setValue('aaaa')
+    let errors = zoneCourriel.errors || {};
+    expect(errors["pattern"]).toBeFalsy();
+  });
+  it('#25 | Zone ADRESSE COURRIEL sans valeur et Zone CONFIRMER COURRIEL avec valeur valide retourne null', () =>{
+    component.gestionNotification('courriel');
+    let zoneCourriel = component.problemeForm.get('courrielGroup')
+    let zoneConfirmerCourriel = component.problemeForm.get('courrielGroup.courrielConfirmation')
+    zoneConfirmerCourriel.setValue('asdf@gmail.com')
+    expect(zoneCourriel.invalid).toBeFalse();
+  });
+  it('#26 | Zone ADRESSE COURRIEL avec valeur valide et Zone CONFIRMER COURRIEL sans valeur retourne null', () =>{
+    component.gestionNotification('courriel');
+    let zoneCourriel = component.problemeForm.get('courrielGroup.courriel')
+    zoneCourriel.setValue('asdf@gmail.com')
+    let zoneConfirmerCourriel = component.problemeForm.get('courrielGroup.courrielConfirmation')
+    expect(zoneConfirmerCourriel.invalid).toBeFalse();
+  });
+  it('#27 | Zones ADRESSE COURRIEL et CONFIRMER COURRIEL sont invalides si les valeurs sont différentes quand notifier par courriel'
+  , () =>{
+    component.gestionNotification('courriel');
+    let zoneConfirmerCourriel = component.problemeForm.get('courrielGroup.courrielConfirmation')
+    zoneConfirmerCourriel.setValue('abb@gmail.com')
+    let zoneCourriel = component.problemeForm.get('courrielGroup.courriel')
+    let group = component.problemeForm.get('courrielGroup')
+    zoneCourriel.setValue('abbc@gmail.com')
+    expect(group.valid).toBeFalse();
+  });
+  it('#28 | Zones ADRESSE COURRIEL et CONFIRMER COURRIEL sont valides si les valeurs sont identiques quand notifier par courriel'
+  , () =>{
+    component.gestionNotification('courriel');
+    let zoneConfirmerCourriel = component.problemeForm.get('courrielGroup.courrielConfirmation')
+    zoneConfirmerCourriel.setValue('abbc@gmail.com')
+    let zoneCourriel = component.problemeForm.get('courrielGroup.courriel')
+    let group = component.problemeForm.get('courrielGroup')
+    zoneCourriel.setValue('abbc@gmail.com')
+    expect(group.invalid).toBeFalse();
+  });
+  
 
 });
-
-
-
--- src/app/probleme/probleme.component.ts
+++ src/app/probleme/probleme.component.ts
@@ -14,20 +14,78 @@ export class ProblemeComponent implements OnInit {
   typesProbleme: ITypeProbleme[];
   errorMessage: string;
 
-  constructor(private fb: FormBuilder, private typeproblemeService : ProblemeService) { }
+  constructor(
+    private fb: FormBuilder,
+    private typeproblemeService: ProblemeService
+  ) {}
 
   ngOnInit() {
-    this.problemeForm = this.fb.group( {
-      prenom: ['', [VerifierCaracteresValidator.longueurMinimum(3), Validators.required]],
-      nom: ['', [VerifierCaracteresValidator.longueurMinimum(3), Validators.required]],
-      noTypeProbleme: ["", Validators.required]
+    this.problemeForm = this.fb.group({
+      prenom: [
+        '',
+        [VerifierCaracteresValidator.longueurMinimum(3), Validators.required],
+      ],
+      nom: [
+        '',
+        [VerifierCaracteresValidator.longueurMinimum(3), Validators.required],
+      ],
+      noTypeProbleme: ['', Validators.required],
+      courrielGroup: this.fb.group({
+        courriel: [{ value: '', disabled: true }],
+        courrielConfirmation: [{ value: '', disabled: true }],
+      }),
+      telephone: [{ value: '', disabled: true }],
     });
 
-    this.typeproblemeService.obtenirTypesProbleme()
-        .subscribe(typesProbleme => this.typesProbleme = typesProbleme,
-                   error => this.errorMessage = <any>error);  
-
-
+    this.typeproblemeService.obtenirTypesProbleme().subscribe(
+      (typesProbleme) => (this.typesProbleme = typesProbleme),
+      (error) => (this.errorMessage = <any>error)
+    );
   }
   save(): void {}
+
+  gestionNotification(typeNotification: string): void {
+    const courrielControl = this.problemeForm.get('courrielGroup.courriel');
+    const courrielConfirmationControl = this.problemeForm.get('courrielGroup.courrielConfirmation');   
+    const courrielGroupControl = this.problemeForm.get('datesGroup');   
+    
+    const telephoneControl = this.problemeForm.get('telephone');   
+
+
+    // Tous remettre à zéro
+    courrielControl.clearValidators();
+    courrielControl.reset();  // Pour enlever les messages d'erreur si le controle contenait des données invaldides
+    courrielControl.disable();  
+
+    courrielConfirmationControl.clearValidators();
+    courrielConfirmationControl.reset();    
+    courrielConfirmationControl.disable();
+
+    telephoneControl.clearValidators();
+    telephoneControl.reset();    
+    telephoneControl.disable();
+
+    if (typeNotification === 'Courriel') {   
+      //courrielControl.setValidators([Validators.required]);      
+      //courrielControl.enable();  
+      //dateExpeditionControl.setValidators([Validators.required]);              
+      //dateExpeditionControl.enable();  
+      // Si le validateur est dans un autre fichier l'écire sous la forme suivante : 
+      // ...Validators.compose([classeDuValidateur.NomDeLaMethode()])])
+      //datesGroupControl.setValidators([Validators.compose([datesValides])]);                       
+}   
+else
+{
+  if(typeNotification === 'Téléphone')
+  {
+    telephoneControl.setValidators([Validators.required]);      
+    telephoneControl.disable();           
+  }
+}
+
+
+courrielControl.updateValueAndValidity();   
+courrielConfirmationControl.updateValueAndValidity();         
+telephoneControl.updateValueAndValidity();
+  }
 }
